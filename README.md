c05ba3438acbdf06c056cb7fc5c7edaa55075512
Kj <46800811+kj-9@users.noreply.github.com>
2024-09-07 19:20:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# git-scraping
+
+Git-scraping data from the Japanese Meteorological Agency (JMA).
+
+- Scrapes daily forcast of min / max temperarture of mesh in geojson format
---
+++ scrape.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+# Set timezone to JST
+export TZ="Asia/Tokyo"
+
+# today and tomorrow
+START_DATE=$(date +%Y%m%d)
+END_DATE=$(date -v +1d +%Y%m%d)
+
+echo "START_DATE: ${START_DATE}"
+echo "END_DATE: ${END_DATE}"
+
+# seems fixed
+START_TIME="080000"
+END_TIME="000000"
+
+for TARGET in min_temp_point max_temp_point
+do
+  echo "TARGET: ${TARGET}"
+  URL="https://www.jma.go.jp/bosai/jmatile/data/wdist/${START_DATE}${START_TIME}/none/${END_DATE}${END_TIME}/surf/${TARGET}/data.geojson?id=${TARGET}"
+  
+  echo "fetch URL: ${URL}"
+  
+  DIR="data/${START_DATE}${START_TIME}-${END_DATE}${END_TIME}"
+  mkdir -p $DIR
+
+  curl -s $URL |
+    jq . > "${DIR}/${TARGET}.geojson"
+
+  echo "saved to ${DIR}/${TARGET}.geojson"
+done
