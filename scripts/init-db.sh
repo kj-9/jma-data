0b9b485b9de44785f473a4dc602d33a7ea41a7dc
Kj <46800811+kj-9@users.noreply.github.com>
2024-09-10 08:55:53 +0900

    refactor  script to initialize SQLite database and create necessary tables, refs #3

---
+++ scripts/init-db.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# constants
+FILE_DB="data/jma.db"
+
+# init db if not exists
+# if there is not $FILE_DB, create it
+if [ ! -f $FILE_DB ]; then
+    sqlite-utils create-database $FILE_DB --init-spatialite
+fi
+
+
+# create tables
+echo "create if not exists \`times\` table..."
+sqlite-utils $FILE_DB '
+create table if not exists times (
+    time_id INTEGER PRIMARY KEY not null,
+    base_time TEXT not null,
+    valid_time TEXT not null,
+    unique (base_time, valid_time)
+);'
+
+echo "create if not exists \`points\` table..."
+sqlite-utils --load-extension=spatialite $FILE_DB '
+create table if not exists points (
+    point_id INTEGER PRIMARY KEY not null,
+    geometry POINTS not null,
+    unique (geometry)
+);'
+
+
+echo "create if not exists \`min_temp\` table..."
+sqlite-utils --load-extension=spatialite $FILE_DB '
+create table if not exists min_temp (
+    time_id INTEGER not null,
+    point_id INTEGER not null,
+    min_temp not null,
+    primary key (time_id, point_id)
+    foreign key (time_id) references times (time_id)
+    foreign key (point_id) references points (point_id)
+);'
+
+echo "create if not exists \`max_temp\` table..."
+sqlite-utils --load-extension=spatialite $FILE_DB '
+create table if not exists max_temp (
+    time_id INTEGER not null,
+    point_id INTEGER not null,
+    max_temp not null,
+    primary key (time_id, point_id)
+    foreign key (time_id) references times (time_id)
+    foreign key (point_id) references points (point_id)
+);'
---
+++ scripts/upsert.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+# constants
+FILE_DB="data/jma.db"
+
+# args
+FILE_GEOJSON=$1 # "data/tmp/20240909080000-20240910000000-min_temp_point.geojson"
+
+# drived
+# min_temp
+TABLE_APPEND=$(basename $FILE_GEOJSON | cut -d'-' -f3 | cut -d'.' -f1 | sed 's/_point//')
+TABLE_INGEST="${TABLE_APPEND}_ingest"
+
+# 20240909080000
+BASETIME=$(basename $FILE_GEOJSON | cut -d'-' -f1)
+# 20240910000000
+VALIDTIME=$(basename $FILE_GEOJSON | cut -d'-' -f2)
+
+echo "TABLE_INGEST: $TABLE_INGEST"
+echo "BASETIME: $BASETIME"
+echo "VALIDTIME: $VALIDTIME"
+
+# for import geojson using spatialite
+export SPATIALITE_SECURITY=relaxed
+
+echo "upsert to \`times\`..."
+sqlite-utils $FILE_DB '
+insert or ignore into times (base_time, valid_time)
+values (:base_time, :valid_time)' \
+    -p base_time $BASETIME -p valid_time $VALIDTIME
+
+
+echo "load geojson to \`$TABLE_INGEST\`..."
+sqlite-utils --load-extension=spatialite \
+    $FILE_DB "select ImportGeoJSON('$FILE_GEOJSON', '$TABLE_INGEST')" \
+
+#sqlite-utils $FILE_DB "select * from $TABLE_INGEST limit 10"
+
+
+
+echo "upsert to \`points\`..."
+q=$(cat <<EOF
+insert or ignore into points(geometry)
+select geometry from $TABLE_INGEST;
+EOF
+)
+sqlite-utils $FILE_DB "$q"
+#sqlite-utils $FILE_DB "select * from points limit 10"
+
+
+echo "upsert to \`$TABLE_APPEND\`..."
+q=$(cat <<EOF
+insert into $TABLE_APPEND
+select times.time_id, points.point_id, value
+from $TABLE_INGEST
+  left join points using (geometry)
+  cross join (select time_id from times where base_time=:base_time and valid_time=:valid_time) as times
+where true
+ON CONFLICT(time_id, point_id) DO UPDATE SET $TABLE_APPEND=excluded.$TABLE_APPEND;
+EOF
+)
+sqlite-utils $FILE_DB "$q" -p base_time $BASETIME -p valid_time $VALIDTIME
+#sqlite-utils $FILE_DB "select * from $TABLE_APPEND order by $TABLE_APPEND limit 1";
+
+
+echo "cleanup..."
+echo "drop table \`$TABLE_INGEST\`..."
+sqlite-utils --load-extension=spatialite $FILE_DB "select DropTable('main', '$TABLE_INGEST')"
+
+echo "remove $FILE_GEOJSON..."
+rm -f $FILE_GEOJSON
+
+echo "vacuum db..."
+sqlite-utils vacuum $FILE_DB
+
+echo "file size:"
+ls -lah $FILE_DB
