58c38d2f3e6ea208add93a1d94ff4f124a2b7cf7
Kj <46800811+kj-9@users.noreply.github.com>
2024-09-16 15:28:51 +0900

    bit of refactor

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -9,21 +9,23 @@ echo "get geojson ..."
 bash scripts/scrape/get-geojson.sh
 
 echo "upsert geojson ..."
-du -h data/jma.db.gz
+
+du -h $FILE_DB_GZ
 
 echo "gunzip ${FILE_DB_GZ} ..."
 gunzip -fk $FILE_DB_GZ
 
-du -h data/jma.db.gz
+du -h $FILE_DB
+
 
 find data/tmp -type f -name "*.geojson" | xargs -I {} bash scripts/scrape/upsert.sh {}
 
 echo "vacuum db..."
 sqlite-utils vacuum $FILE_DB
 
-du -h data/jma.db.gz
+du -h $FILE_DB
 
 echo "gzip ${FILE_DB} ..."
 gzip -f $FILE_DB
 
-du -h data/jma.db.gz
+du -h $FILE_DB_GZ
